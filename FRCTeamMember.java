e234cb3ca73415a14d6888f258ffc0f24e576058
RJBarberis <rj.barberis@student.csd509j.net>
2025-12-02 09:13:06 -0800

    Removed Main from FRCTeamMember

--- FRCTeamMember.java
+++ FRCTeamMember.java
@@ -54,24 +54,4 @@ public class FRCTeamMember
     public void addHours(double moreHours) {
         hours += moreHours;
     }
-
-    public static void main(String[] args) {
-        FRCTeamMember m1 = new FRCTeamMember("Aidan", "Programming", 42.5);
-        FRCTeamMember m2 = new FRCTeamMember("Oskar", "Electrical", 30.0);
-
-        m1.print();
-        m2.print();
-
-        System.out.println("m1 Name: " + m1.getName());
-        System.out.println("m2 Subteam: " + m2.getSubteam());
-
-        m2.setSubteam("Fabrication");
-        System.out.println("m2 New Subteam: " + m2.getSubteam());
-
-        m1.addHours(3.5);
-        System.out.println("m1 New Hours: " + m1.getHours());
-
-        System.out.println(m1.toString());
-        System.out.println(m2.toString());
-    }
 }
