67fc617303b7b5f9c1c755a2ca5716650741d743
RJBarberis <rj.barberis@student.csd509j.net>
2025-12-02 09:13:58 -0800

    added main to Main

--- Main.java
+++ Main.java
@@ -1,8 +1,22 @@
 class Main {
 
-  public static void main(String[] args) {
-    // Construct 2 objects of your class using the constructor with different values
+    public static void Main(String[] args) {
+        FRCTeamMember m1 = new FRCTeamMember("Aidan", "Programming", 42.5);
+        FRCTeamMember m2 = new FRCTeamMember("Oskar", "Electrical", 30.0);
 
-    // call all of the objects methods to test them
-  }
+        m1.print();
+        m2.print();
+
+        System.out.println("m1 Name: " + m1.getName());
+        System.out.println("m2 Subteam: " + m2.getSubteam());
+
+        m2.setSubteam("Fabrication");
+        System.out.println("m2 New Subteam: " + m2.getSubteam());
+
+        m1.addHours(3.5);
+        System.out.println("m1 New Hours: " + m1.getHours());
+
+        System.out.println(m1.toString());
+        System.out.println(m2.toString());
+    }
 }
